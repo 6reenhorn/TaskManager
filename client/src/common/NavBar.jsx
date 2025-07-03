b144877caa6a4524af09446b3674ec70575d8d3f
6reenhorn <coding.cyrilj777@gmail.com>
2025-07-03 10:52:53 +0800

    Navbar btn update

--- client/src/common/NavBar.jsx
+++ client/src/common/NavBar.jsx
@@ -4,7 +4,7 @@ export const NavBar = ({ isSidebarOpen, toggleSidebar }) => {
     return (
         <div className="relative w-full">
             <div className="navbar w-full py-5 px-4 pr-7 flex justify-between items-center">
-                <div className="cursor-pointer flex justify-between items-center space-x-2 w-[25.4%]">
+                <div className="cursor-pointer flex justify-between items-center space-x-2 w-[23%]">
                     <svg onClick={toggleSidebar} width="30" height="30" viewBox="0 0 24 24" fill="none">
                         <path d="M4 9C3.58579 9 3.25 9.33579 3.25 9.75C3.25 10.1642 3.58579 10.5 4 10.5L20 10.5C20.4142 10.5 20.75 10.1642 20.75 9.75C20.75 9.33579 20.4142 9 20 9L4 9Z" fill="#343C54"/>
                         <path d="M4 18C3.58579 18 3.25 18.3358 3.25 18.75C3.25 19.1642 3.58579 19.5 4 19.5L20 19.5C20.4142 19.5 20.75 19.1642 20.75 18.75C20.75 18.3358 20.4142 18 20 18L4 18Z" fill="#343C54"/>
